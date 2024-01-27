92f3f21c68f8efa446855467d739518942c97390
Alvaro Augusto do Nascimento Filho <alvarosum@hotmail.com>
2024-01-27 18:50:06 -0300

    create service and password encryption

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,5 +1,11 @@
+import bcrypt from "bcrypt"
+import authRepository from "../repositories/authRepository.js"
+
 function singup(body) {
-  return body;
+  const hasPassord = bcrypt.hashSync(body.password,10)
+
+  return authRepository.create({...body, password:hasPassord})
+  
 }
 
 export default { singup };
