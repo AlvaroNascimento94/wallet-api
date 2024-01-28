0ee944a603b35822adc82d7009074fa4adc9f932
Alvaro Augusto do Nascimento Filho <alvarosum@hotmail.com>
2024-01-28 17:35:35 -0300

    middleware JWT

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -1,14 +1,14 @@
-import transactionService from "../services/transactionService.js"
+import transactionService from "../services/transactionService.js";
 
-async function create(req, res){
-    const body = req.body
-    const id = "65b5806e1cb6f33211808e03"
-    try {
-        const transaction = await transactionService.create(body,id)
-        return res.status(201).send(transaction )
-    } catch (err) {
-        res.status(409).send(err.message)
-    }
+async function create(req, res) {
+  const body = req.body;
+  const { _id: id } = res.locals.user;
+  try {
+    const transaction = await transactionService.create(body, id);
+    return res.status(201).send(transaction);
+  } catch (err) {
+    res.status(409).send(err.message);
+  }
 }
 
-export default {create}
+export default { create };
--- src/repositories/authRepository.js
+++ src/repositories/authRepository.js
@@ -2,15 +2,21 @@ import UserShema from "../shemas/User.js";
 import jwt from "jsonwebtoken";
 
 async function create(data) {
-  return await UserShema.create(data) ;
+  return await UserShema.create(data);
 }
 
 async function findByEmail(email) {
-  const user = await UserShema.findOne({ email: email });
+  const user = await UserShema.findOne({ email });
   return user;
 }
 
-async function generateToken(id){
-    return jwt.sign({id}, process.env.SECRET, {expiresIn: 86400});
+async function generateToken(id) {
+  return jwt.sign({ id }, process.env.SECRET, { expiresIn: 86400 });
 }
-export default { create, findByEmail, generateToken };
+
+async function findById(id) {
+  const user = await UserShema.findById(id);
+  return user;
+}
+
+export default { create, findByEmail, generateToken, findById };
